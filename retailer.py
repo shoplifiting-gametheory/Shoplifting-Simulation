c734b768cf439f474ed7177abdc69ca00de2936d
chingchinghuang <huang27c@mtholyoke.edu>
2019-04-29 14:03:19 -0400

    stratgey method

--- retailer.py
+++ retailer.py
@@ -2,3 +2,11 @@
 #Chooses level of security vs profit made off of stuff that was not stolen
 
 class Retailer:
+
+	'''
+	Retailer decides the amount to spend on security base on the stolen rate
+	parameter: the amount of items that are stolen from thieves
+	return: the amount to spend on security, stolen amount * 0.8
+	'''
+	def strategy(self, stolenAmount):
+		return stolenAmount*0.8
