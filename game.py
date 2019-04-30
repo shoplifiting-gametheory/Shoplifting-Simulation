f96c76b0f75b80a99bccc099ab55d87723a93cea
leshghi <43756305+leshghi@users.noreply.github.com>
2019-04-30 13:26:06 -0400

    Add files via upload

--- game.py
+++ game.py
@@ -5,23 +5,20 @@ from supplier import Supplier
 thief = Thief()
 retailer = Retailer()
 supplier = Supplier()
-tpoints = thief.points()
 
 #Suppose the game starts from low security level and stealing $50
 i = 0
 while i < 10:
-        stolenAmount, nextRound = thief.steal(50, 30)
-        security = retailer.strategy(stolenAmount)
-        securityLevel, profit = supplier.strategy(security, stolenAmount)
-        if stolenAmount < 0:
+	stolenAmount, nextRound = thief.steal(50, 30)
+	security = retailer.strategy(stolenAmount)
+	securityLevel, profit = supplier.strategy(security, stolenAmount)
+	if stolenAmount < 0:
                 print("Thief was caught and paid ${} this round and will steal ${} next round".format(stolenAmount, nextRound))
         else:
                 print("Thief stole ${} this round and will steal ${} nextRound".format(stolenAmount, nextRound))
-        
-
         print("Retailer spends ${} on security".format(security))
-        print("Security Supplier has profit of ${} and provides {}% level of security".format(profit, securityLevel))
-        i += 1
+	print("Security Supplier has profit of ${} and provides {}% level of security".format(profit, securityLevel))
+	i += 1
 
 tpoints = thief.points()
 rpoints = retailer.points()
